4c7553d15ed285b171c805e148b9f5d626edaf3f
Lucas Lattari <lucas.lattari@ifsudestemg.edu.br>
2020-04-07 11:10:33 -0300

    small fixes

--- src/04_other_basic_operations.py
+++ src/04_other_basic_operations.py
@@ -7,37 +7,30 @@ def showImage(img):
     plt.imshow(imgMPLIB)
     plt.show()
 
-def showImageGrid(img, title):
-    fig, axis = plt.subplots()
-    imgMPLIB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    axis.imshow(imgMPLIB)
-    axis.set_title(title)
-    plt.show()
-
 def showMultipleImageGrid(imgsArray, titlesArray, x, y):
-    if(x < 1 and y < 1):
-        print("ERRO: X e Y não podem ser zero simultaneamente!")
+    if(x < 1 or y < 1):
+        print("ERRO: X e Y não podem ser zero ou abaixo de zero!")
         return
-    elif(x == 1 or y == 1):
-        showImageGrid(imgsArray[0], titlesArray[0])
-    elif(x == 0):
+    elif(x == 1 and y == 1):
+        showImageGrid(imgsArray, titlesArray)
+    elif(x == 1):
         fig, axis = plt.subplots(y)
         fig.suptitle(titlesArray)
-        axisId = 0
+        yId = 0
         for img in imgsArray:
             imgMPLIB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            axis[axisId].imshow(imgMPLIB)
+            axis[yId].imshow(imgMPLIB)
 
-            axisId += 1
-    elif(y == 0):
+            yId += 1
+    elif(y == 1):
         fig, axis = plt.subplots(1, x)
         fig.suptitle(titlesArray)
-        axisId = 0
+        xId = 0
         for img in imgsArray:
             imgMPLIB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            axis[axisId].imshow(imgMPLIB)
+            axis[xId].imshow(imgMPLIB)
 
-            axisId += 1
+            xId += 1
     else:
         fig, axis = plt.subplots(y, x)
         xId, yId, titleId = 0, 0, 0
@@ -62,24 +55,14 @@ def plotSingleImage():
     imgOriginal = cv2.imread("ada.jpg")
     showImageGrid(imgOriginal, "Foto da Ada")
 
-def plotTwoImageHorizontal():
-    imgOriginal = cv2.imread("ada.jpg")
-    imgReplicate = cv2.copyMakeBorder(imgOriginal, 100, 100, 100, 100, cv2.BORDER_REPLICATE)
-
-    #criando grid com 2 imagens, a segunda com borda replicada
-    imgsArray = [imgOriginal, imgReplicate]
-    titlesArray = 'Imagem Original e Imagem com Borda Replicada'
-    showMultipleImageGrid(imgsArray, titlesArray, 0, 2)
-    #showMultipleImageGrid(imgsArray, titlesArray, 2, 0)
-
 def plotTwoImageVertical():
     imgOriginal = cv2.imread("ada.jpg")
-    imgReplicate = cv2.copyMakeBorder(imgOriginal, 100, 100, 100, 100, cv2.BORDER_REPLICATE)
+    imgReplicate = cv2.copyMakeBorder(imgOriginal, 200, 100, 50, 10, cv2.BORDER_REPLICATE)
 
     #criando grid com 2 imagens, a segunda com borda replicada
     imgsArray = [imgOriginal, imgReplicate]
-    titlesArray = 'Imagem Original e Imagem com Borda Replicada'
-    showMultipleImageGrid(imgsArray, titlesArray, 2, 0)
+    title = 'Imagem Original e Imagem com Borda Replicada'
+    showMultipleImageGrid(imgsArray, title, 1, 2)
 
 def plotThreeImages():
     imgOriginal = cv2.imread("ada.jpg")
@@ -90,7 +73,7 @@ def plotThreeImages():
     #criando grid com 3 imagens, a segunda com borda replicada e a terceira com borda de espelho
     #a ultima imagem é transparente
     imgsArray = [imgOriginal, imgReplicate, imgReflect, imgTransparent]
-    titlesArray = ['Imagem Original', 'Imagem com Borda Replicada', 'Imagem com Borda de Espelho', '']
+    titlesArray = ['Original', 'Borda Replicada', 'Borda de Espelho', '']
     showMultipleImageGrid(imgsArray, titlesArray, 2, 2)
 
 def plotFourImages():
@@ -101,7 +84,7 @@ def plotFourImages():
 
     #criando grid com 4 imagens, a segunda com borda replicada e a terceira e quarta com borda de espelho
     imgsArray = [imgOriginal, imgReplicate, imgReflect, imgReflect101]
-    titlesArray = ['Imagem Original', 'Imagem com Borda Replicada', 'Imagem com Borda de Espelho', 'Imagem com Borda de Espelho Diferente']
+    titlesArray = ['Original', 'Borda Replicada', 'Borda de Espelho', 'Borda de Espelho 2']
     showMultipleImageGrid(imgsArray, titlesArray, 2, 2)
 
 def plotSixImages():
@@ -119,6 +102,22 @@ def plotSixImages():
     imgsArray = [imgOriginal, imgReplicate, imgReflect, imgReflect101, imgConstant, imgWrap]
     titlesArray = ['Original', 'Borda Replicada', 'Borda de Espelho', 'Borda de Espelho 2', 'Moldura', 'Efeito Wrap']
     showMultipleImageGrid(imgsArray, titlesArray, 3, 2)
+
+def showImageGrid(img, title):
+    fig, axis = plt.subplots()
+    imgMPLIB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    axis.imshow(imgMPLIB)
+    axis.set_title(title)
+    plt.show()
+
+def plotTwoImageHorizontal():
+    imgOriginal = cv2.imread("ada.jpg")
+    imgReplicate = cv2.copyMakeBorder(imgOriginal, 200, 100, 50, 25, cv2.BORDER_REPLICATE)
+
+    #criando grid com 2 imagens, a segunda com borda replicada
+    imgsArray = [imgOriginal, imgReplicate]
+    title = 'Imagem Original e Imagem com Borda Replicada'
+    showMultipleImageGrid(imgsArray, title, 2, 1)
 
 def main():
     plotSixImages()
